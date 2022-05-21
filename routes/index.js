7bb1c03148a1e9f955affccb112bb7563678b946
KaitoYamaki <105027673+KaitoYamaki@users.noreply.github.com>
2022-05-21 22:44:25 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -20,17 +20,38 @@ router.get('/contact_form', function(req, res, next) {
 
 router.post('/contacts', async function(req, res, next) {
   try {
-    console.log('posted', req.body);
-    const contact = models.Contact.build({ name: req.body.name, email: req.body.email });
-    await contact.save();
+    if(req.body.id){
+      const contact = await models.Contact.findByPk(req.body.id);
+      contact.name= req.body.name;
+      contact.email= req.body.email;
+      await contact.save();
+      req.session.flashMessage= `「${contact.name}」さんを更新しました`;
+    }else{
+      const contact = models.Contact.build({ name: req.body.name, email: req.body.email });
+      await contact.save();
+      req.session.flashMessage = `新しい連絡先として「${contact.name}」さんを保存しました`;
+    }
     res.redirect('/');
   } catch (err) {
     if(err instanceof ValidationError) {
-      res.render(`contact_form`, { title: '連絡先フォーム', contact: req.body, err: err });
+      const title = (req.body.id) ? '連絡先の更新' : '連絡先の作成'; //--- [5〜]
+      res.render(`contact_form`, { title, contact: req.body, err: err }); //--- [〜5]
     } else {
       throw err; // ここでの対応を諦めて処理系に任せる
     }
   }
 });
+
+router.get('/contacts/:id/edit', async function(req, res, next) {
+  const contact = await models.Contact.findByPk(req.params.id);
+  res.render('contact_form', {title: '連絡先の更新', contact});
+});
+
+router.post('/contacts/:id/delete', async function(req, res, next){
+  const contact = await models.Contact.findByPk(req.params.id)
+  await contact.destroy();
+  req.session.flashMessage = `「${contact.name}」さんを削除しました`;
+  res.redirect('/');
+})
 
 module.exports = router;
